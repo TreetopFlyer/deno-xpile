247a4cd8cb9859037960c2a1470b0d0aba6e2e38
Seth Trowbridge <seth111@gmail.com>
2022-08-31 20:49:23 -0400

    useIsoData cleaned up

--- app/App.tsx
+++ app/App.tsx
@@ -13,7 +13,11 @@ export type PreloadMetas = {
     canonical?: string,
     image?: string
 }
-export type PreloadEntry = {data:false|string, error:boolean|string, promise:false|Promise<PreloadEntry>}
+export type PreloadEntry = {
+    data:false|string,
+    error:boolean|string,
+    promise:false|Promise<PreloadEntry>
+}
 export type PreloadTable = {
     meta: PreloadMetas,
     data:
@@ -24,10 +28,7 @@ export type PreloadTable = {
     queue: Promise<PreloadEntry>[],
     client: boolean
 };
-export type PreloadInterface =
-{
-    data: (inURL:string)=>PreloadEntry
-};
+
 export const PreloadObject:PreloadTable = {
     meta: {},
     data: {},
@@ -36,62 +37,51 @@ export const PreloadObject:PreloadTable = {
     client: true
 };
 
-export const PreloadMethods:PreloadInterface =
-{
-    data(inURL)
-    {
-        const slot:PreloadEntry|undefined = PreloadObject.data[inURL];
-        if(slot)
-        {
-            return slot;
-        }
-        else
-        {
-            const newSlot = {data:false, error:false, promise:false} as PreloadEntry;
-            PreloadObject.data[inURL] = newSlot;
-
-            const loader = async (inURL:string, inSlot:PreloadEntry)=>
-            {
-                try
-                {
-                    const response = await fetch(inURL);
-                    const text = await response.text();
-                    response.status == 200 ? inSlot.data=text : inSlot.error=text;
-                }
-                catch(e){ inSlot.error = e;}
-
-                newSlot.promise = false;
-                return newSlot;
-            };
-
-            newSlot.promise = loader(inURL, newSlot);
-            if(PreloadObject.client){ PreloadObject.queue.push(newSlot.promise); }
-
-            return newSlot;
-        }
-    }
-};
-
 export const useIsoData =(inURL:string)=>
 {
-    const slot = PreloadMethods.data(inURL);
-    const [getSlot, setSlot] = React.useState(slot);
+    let slot:PreloadEntry|undefined = PreloadObject.data[inURL];
+    if(!slot)
+    {
+        slot = {data:false, error:false, promise:false} as PreloadEntry;
+        PreloadObject.data[inURL] = slot;
+
+        const loader = async (inURL:string, inSlot:PreloadEntry)=>
+        {
+            try
+            {
+                const response = await fetch(inURL);
+                const text = await response.text();
+                response.status == 200 ? inSlot.data=text : inSlot.error=text;
+            }
+            catch(e){ inSlot.error = e;}
+
+            inSlot.promise = false;
+            return inSlot;
+        };
+
+        slot.promise = loader(inURL, slot);
+        if(!PreloadObject.client){ PreloadObject.queue.push(slot.promise); }
+    }
+    const [slotGet, slotSet] = React.useState(slot);
     React.useEffect(()=>
     {
-        if(slot.promise)
+        if(slot)
         {
-            console.log(`resource is currently being fetched`);
-            slot.promise.then(()=>setSlot(slot)).then(()=>console.log(`resource loaded`));
-        }
-        else
-        {
-            console.log(`preloaded found (${inURL}), switching states`);
-            setSlot(slot);
+            if(slot.promise)
+            {
+                console.log(`resource is currently being fetched`);
+                slot.promise.then(inSlot=>{slotSet(inSlot); console.log(`resource loaded`)});
+            }
+            else
+            {
+                console.log(`preloaded found (${inURL}), switching states`);
+                slotSet(slot);
+            }
         }
 
     }, [inURL]);
 
-    return { data: getSlot.data, error: getSlot.error, json: getSlot.data ? JSON.parse(getSlot.data) : false };
+    return { data: slotGet.data, error: slotGet.error, json: slotGet.data ? JSON.parse(slotGet.data) : false };
 };
 
 export const useIsoMeta =():[PreloadMetas, (update:PreloadMetas)=>void]=>

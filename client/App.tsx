98baf9e00b277644dd5cc639951fa22b0a8b324a
Seth Trowbridge <seth111@gmail.com>
2022-09-11 09:08:33 -0400

    working

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Deep from "./Deep.tsx"; 
 
+const dep = "DEPe"
+
 export default ()=>
 {
-    const [countGet, countSet] = React.useState(3);
+    const [countGet, countSet] = React.useState(2);
+    console.log("app rerender!")
+
     return <div>
-        <h2>le app?</h2>
+        <h2>le app! {dep}</h2>
+        
         <div className="border-4 no-t">
             <button onClick={()=>countSet(countGet+1)}>{countGet}</button>
         </div>
--- source/AmberServer.tsx
+++ source/AmberServer.tsx
@@ -8,12 +8,79 @@ import * as TwindServer from "https://esm.sh/twind/shim/server";
 import { type State, PathParse, IsoProvider } from "./AmberClient.tsx";
 import MIMELUT from "./mime.json" assert {type:"json"};
 
+const dir = Deno.cwd();
+const resp404 = new Response("404", {status:404, headers:{"content-type": "application/javascript; charset=utf-8"}});
+const FilesParse:{[key:string]:string} = {};
+let socketRef:undefined|WebSocket;
+
+const Rebuild =async(Themed:string, Client:string, Source:string):Promise<void>=>
+{
+    let twindImport = { default:{theme:{}, plugins:{}} };
+    const twindPath = `file://${dir}/${Themed}`;
+    try
+    {
+        twindImport = await import(twindPath);
+        console.log(`twind found at (${twindPath})`);
+    }
+    catch(e) { console.log(`no twind config found at (${twindPath}), using defaults.`); }
+    const sheet = TwindServer.virtualSheet();
+    const parse = Twind.create({ sheet: sheet, preflight: true, theme: twindImport.default?.theme??{}, plugins: twindImport.default?.plugins??{}, mode: "silent" }).tw;
+    const leave = [ "__defineGetter__", "__defineSetter__", "__lookupGetter__", "__lookupSetter__", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "valueOf", "toLocaleString" ];
+    
+    const xpile =async(inFolder:string):Promise<void>=>
+    {
+        console.log(`transpiling to memory everything in ${dir+"/"+inFolder}`);
+        for await (const filePath of FS.walk(dir+"/"+inFolder, {exts:["js", "jsx", "ts", "tsx"], includeDirs:false}))
+        {
+            try
+            {
+                const fileText = await Deno.readTextFile(filePath.path);
+                const code:{code:string} = await ESBuild.transform(fileText, {loader:"tsx", /*sourcemap:"inline",*/ minify:true});
+                FilesParse[filePath.path.replaceAll("\\", "/").substring(dir.length+1)] = code.code;
+                const m = code.code.match(/[^<>\[\]\(\)|&"'`\.\s]*[^<>\[\]\(\)|&"'`\.\s:]/g);
+                if (m)
+                {
+                    for (const c of m)
+                    {
+                        if (leave.indexOf(c) === -1)
+                        {
+                            try { parse(c); }
+                            catch (e) { console.log(`Error: Failed to handle the pattern '${c}'`); }
+                        }
+                    }
+                }
+            }
+            catch(e){ console.log(`error transpiling ${filePath.path}`) }
+        }
+    }
+
+    try
+    {
+        await xpile(Client);
+    }
+    catch(e)
+    {
+        console.log(`No client directory "${Client}" found in "${dir}. No front-end code to work with!"`)
+    }
+    
+    try
+    {
+        await xpile(Source);
+        console.log(`Note you have a "source" directory "${Source}" in "${dir}. Are you working on Amber Core?"`)
+    }
+    catch(e)
+    {
+        console.log(`No "source" directory "${Source}" found in "${dir}. You are probably creating an Amber app, not working on Amber itself."`);
+    }
+    
+    FilesParse["static/tailwind.css"] = TwindServer.getStyleTagProperties(sheet).textContent;
+
+    console.log("Transpiling/tailwind complete");
+    socketRef?.send("refresh");
+}
+
 export default async({Themed, Source, Static, Client, Launch, Import, Deploy}:{Themed:string, Source:string, Static:string, Client:string, Launch:string, Import:string, Deploy:number})=>
 {
-    const dir = Deno.cwd();
-    const resp404 = new Response("404", {status:404, headers:{"content-type": "application/javascript; charset=utf-8"}});
-    const FilesParse:{[key:string]:string} = {};
-
     // open amber client *on the remote* and transpile it for the browser
     const parts = import.meta.url.split("/");
     parts[parts.length-1] = "AmberClient.tsx";
@@ -61,25 +128,41 @@ export default async({Themed, Source, Static, Client, Launch, Import, Deploy}:{T
                 <div id="app" dangerouslySetInnerHTML={{__html:bake}}></div>
                 <script type="module" dangerouslySetInnerHTML={{__html:
         `import {createElement as h} from "react";
-        import {hydrateRoot} from "react-dom/client";
+        import {hydrateRoot, createRoot} from "react-dom/client";
         import App from "./${clientFolder}${launchFile}";
         import { IsoProvider } from "amber";
         
         const iso = ${JSON.stringify(isoModel)};
-    
+                    
+        const dom = document.querySelector("#app");
+
         hydrateRoot(
-            document.querySelector("#app"),
+            dom,
             h(IsoProvider, {seed:iso},
                 h(App)
             )
         );
+
+        const appRoot = createRoot(dom);
+        const socket = new WebSocket('ws://localhost:3333/hmr');
+        socket.addEventListener('message', (event) => {
+
+            console.log('Message from server ', event.data);
+
+            import("./${clientFolder}${launchFile}?cache="+Math.random()).then(module=>
+            {
+                console.log("app module loaded", module);
+                appRoot.render(
+                    
+                    h(IsoProvider, {seed:iso},
+                        h(module.default)
+                    )
+                )
+            });
+
+        });
+
         `}}/>
-<script type="module" dangerouslySetInnerHTML={{__html:`
-const socket = new WebSocket('ws://localhost:3333/hmr');
-socket.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data);
-});
-`}}/>
             </body>
         </html>;
     }
@@ -96,70 +179,8 @@ socket.addEventListener('message', (event) => {
     }
     catch(e) { console.log(e); console.log(`Launch file "${Launch}" cound not be found in Client directory "${appPath}".`); }
 
-    //
-    let twindImport = { default:{theme:{}, plugins:{}} };
-    const twindPath = `file://${dir}/${Themed}`;
-    try
-    {
-        twindImport = await import(twindPath);
-        console.log(`twind found at (${twindPath})`);
-    }
-    catch(e) { console.log(`no twind config found at (${twindPath}), using defaults.`); }
-    const sheet = TwindServer.virtualSheet();
-    const parse = Twind.create({ sheet: sheet, preflight: true, theme: twindImport.default?.theme??{}, plugins: twindImport.default?.plugins??{}, mode: "silent" }).tw;
-    const leave = [ "__defineGetter__", "__defineSetter__", "__lookupGetter__", "__lookupSetter__", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "valueOf", "toLocaleString" ];
-    
-    const xpile =async(inFolder:string):Promise<void>=>
-    {
-        console.log(`transpiling to memory everything in ${dir+"/"+inFolder}`);
-        for await (const filePath of FS.walk(dir+"/"+inFolder, {exts:["js", "jsx", "ts", "tsx"]}))
-        {
-            try
-            {
-                const fileText = await Deno.readTextFile(filePath.path);
-                const code:{code:string} = await ESBuild.transform(fileText, {loader:"tsx", /*sourcemap:"inline",*/ minify:true});
-                FilesParse[filePath.path.replaceAll("\\", "/").substring(dir.length+1)] = code.code;
-                const m = code.code.match(/[^<>\[\]\(\)|&"'`\.\s]*[^<>\[\]\(\)|&"'`\.\s:]/g);
-                if (m)
-                {
-                    for (const c of m)
-                    {
-                        if (leave.indexOf(c) === -1)
-                        {
-                            try { parse(c); }
-                            catch (e) { console.log(`Error: Failed to handle the pattern '${c}'`); }
-                        }
-                    }
-                }
-            }
-            catch(e){ console.log(`error transpiling ${filePath.path}`) }
-        }
-    }
 
-    try
-    {
-        await xpile(Client);
-    }
-    catch(e)
-    {
-        console.log(`No client directory "${Client}" found in "${dir}. No front-end code to work with!"`)
-    }
-    
-    try
-    {
-        await xpile(Source);
-        console.log(`Note you have a "source" directory "${Source}" in "${dir}. Are you working on Amber Core?"`)
-    }
-    catch(e)
-    {
-        console.log(`No "source" directory "${Source}" found in "${dir}. You are probably creating an Amber app, not working on Amber itself."`);
-    }
-    
-    const tailwind = TwindServer.getStyleTagProperties(sheet).textContent;
-
-    console.log("Transpiling/tailwind complete");
-
-    let socketRef:undefined|WebSocket;
+    Rebuild(Themed, Client, Source);
 
     serve(async (inRequest:Request) =>
     {
@@ -223,7 +244,7 @@ socket.addEventListener('message', (event) => {
             
             isoModel.Client = true;
     
-            const page = await ReactDOMServer.renderToReadableStream(<Shell isoModel={isoModel} styles={tailwind} importMap={Import} bake={bake} clientFolder={Client} launchFile={Launch} />);
+            const page = await ReactDOMServer.renderToReadableStream(<Shell isoModel={isoModel} styles={FilesParse["static/tailwind.css"]??""} importMap={Import} bake={bake} clientFolder={Client} launchFile={Launch} />);
             return new Response(page, {status:200, headers:{"content-type": "text/html; charset=utf-8"}});
         }
     }
@@ -232,50 +253,48 @@ socket.addEventListener('message', (event) => {
     console.log("Amber running on port", Deploy);
 
 
-let waiting = false;
-let interrupted = false;
-let timer:undefined|number;
-const timerDone = ()=>
-{
-    
-    if(interrupted)
+    let waiting = false;
+    let interrupted = false;
+    let timer:undefined|number;
+    const timerDone = ()=>
     {
-        console.log("--timer done, but starting again", waiting, interrupted);
-        startTimer();
-    }
-    else
+        
+        if(interrupted)
+        {
+            console.log("--timer done, but starting again", waiting, interrupted);
+            startTimer();
+        }
+        else
+        {
+            waiting = false;
+            interrupted = false;
+            console.log("--timer done", waiting, interrupted);
+            Rebuild(Themed, Client, Source);
+        }
+    };
+    const startTimer =()=>
     {
-        waiting = false;
+        console.log("--timer started", waiting, interrupted);
+        waiting = true;
         interrupted = false;
-        console.log("--timer done", waiting, interrupted);
+        if(timer){clearTimeout(timer);}
 
-        socketRef?.send("refresh");
-
+        timer = setTimeout(timerDone, 500);
     }
-};
-const startTimer =()=>
-{
-    console.log("--timer started", waiting, interrupted);
-    waiting = true;
-    interrupted = false;
-    if(timer){clearTimeout(timer);}
 
-    timer = setTimeout(timerDone, 500);
-}
-
-const registerChange =():boolean=>
-{
-    if(waiting)
+    const registerChange =():boolean=>
     {
-        interrupted = true;
-        return true;
-    }
-    else
-    {
-        startTimer();
-        return false;
-    }
-};
+        if(waiting)
+        {
+            interrupted = true;
+            return true;
+        }
+        else
+        {
+            startTimer();
+            return false;
+        }
+    };
 
     const watcher = Deno.watchFs(".");
     for await (const event of watcher)
